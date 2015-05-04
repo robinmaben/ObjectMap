b1857d0cb59e35bd4c66c7c3632cb8d54047c814
Robin Maben <hello@mabenrob.in>
2015-05-05 00:00:55 +0530

    Fill ctor dependencies; Reverted to C# 5; Added unit tests

--- ObjectMap.Tests/UnitTests.cs
+++ ObjectMap.Tests/UnitTests.cs
@@ -6,8 +6,54 @@ namespace ObjectMap.Tests
     public class UnitTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void WithoutDependency()
         {
+            ObjectMap.Register<IMock, Mock>();
+            var result = ObjectMap.Get<IMock>();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof (Mock));
+        }
+
+        [TestMethod]
+        public void WithCtorDependencies()
+        {
+            ObjectMap.Register<IMock, Mock2>();
+            ObjectMap.Register<IDependency, Dependency>();
+
+            var result = ObjectMap.Get<IMock>();
+
+            Assert.IsInstanceOfType(result, typeof (Mock2));
         }
     }
+
+    public class Mock2 : IMock
+    {
+        public IDependency Dependency { get; set; }
+
+        public Mock2(IDependency dependency)
+        {
+            Dependency = dependency;
+        }
+    }   
+
+    public class Mock : IMock
+    {
+    
+    }
+
+    public class Dependency : IDependency
+    {
+        
+    }
+
+    public interface IMock
+    {
+    }
+
+    public interface IDependency
+    {
+    }
+
+    
 }
--- ObjectMap/ObjectMap.cs
+++ ObjectMap/ObjectMap.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ObjectMap
 {
     public class ObjectMap
     {
+        private static readonly ObjectMap Instance;
         private readonly Dictionary<Type, Func<object>> _objectProviders = new Dictionary<Type, Func<object>>();
         
         static ObjectMap()
@@ -12,31 +14,45 @@ namespace ObjectMap
             Instance = new ObjectMap();
         }
 
-        private static ObjectMap Instance { get; }
-
-        public static IEnumerable<object> Registry => Instance._objectProviders.Values;
-
-        public static void Register<TRequest, TObj>(Func<TObj> provider = null) where TObj : new()
+        public static void Register<TRequest, TObj>() where TObj : class, TRequest where TRequest : class
         {
-            Instance._objectProviders[typeof (TRequest)] = () => provider ?? (() => new TObj());
+            Instance._objectProviders[typeof (TRequest)] = () => CreateInstance(typeof (TObj));
         }
 
-        public static void Register<TObj>(TObj obj)
+        public static void Register<TRequest>(Func<TRequest> provider)
         {
-            Instance._objectProviders[typeof (TObj)] = () => obj;
+            Instance._objectProviders[typeof(TRequest)] = () => provider;
         }
 
-        public static void Register<TRequest, TObj>(TObj obj) where TObj : TRequest
+        public static TRequest Get<TRequest>() where TRequest : class
         {
-            Instance._objectProviders[typeof(TRequest)] = () => obj;
+            return GetInstanceProvider(typeof(TRequest)) as TRequest;
         }
 
-        public static TRequest Get<TRequest>()
+        private static object GetInstanceProvider(Type requestedType)
         {
-            Func<object> obj;
-            Instance._objectProviders.TryGetValue(typeof (TRequest), out obj);
+            Func<object> objectProvider;
 
-            return (TRequest) obj?.Invoke();
+            return Instance._objectProviders.TryGetValue(requestedType, out objectProvider)
+                ? objectProvider.Invoke()
+                : null;
+        }
+
+        private static object CreateInstance(Type type)
+        {
+            var constructor =
+                type.GetConstructors()
+                    .FirstOrDefault(ctor => ctor
+                                .GetParameters()
+                                .All(paramInfo => Instance._objectProviders.ContainsKey(paramInfo.ParameterType)));
+
+            return constructor != null
+                ? constructor.Invoke(constructor.GetParameters().Select(paramInfo => GetInstanceProvider(paramInfo.GetType())).ToArray())
+                : null;
+
+            //TODO: Use public parameterless ctor?
+            //TODO: Fill dependent Properties on requested type
+            //TODO: Prevent circular dependencies
         }
     }
 }
